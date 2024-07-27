2841fa6105b01324c91d84664c896efded6fe839
Name <IceClock@yandex.ru>
2024-07-27 17:40:00 +0300

    Изменения в игре
    Изменились ячейки поля, добавил метод добавления корабля на поле

--- Naval battle.py
+++ Naval battle.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Dot:
     # Класс точки
     def __init__(self, x, y):
@@ -33,17 +36,55 @@ class Board:
         self.size = size
         self.hid = hid
         self.busy = []
-        self.board = [["o"] * size for _ in range(size)]
+        self.board = [["| |"] * size for _ in range(size)]
+        self.shipsunk = 0
+        self.shipalive = 7
+
+    class BoardOutException(Exception):
+        pass
+        # исключение ошибки, когда координаты за пределами доски
+
+    class BoardUsedException(Exception):
+        pass
+        # исключение ошибки, когда координаты используются
+
+    def boardout(self, dot):
+        return not ((0 <= dot.x < self.size) and (0 <= dot.y < self.size))
+
+    def add_ship(self, ship):
+        for dot in ship.dots:
+            if self.boardout(dot) or dot in self.busy:
+                raise BoardOutException()
+        for dot in ship.dots:
+            self.board[dot.x][dot.y] = "■"
+            self.busy.append(dot)
+        self.ships.append(ship)
+        self.lives += ship.length
+        self.contour(ship)
+
+    def contour(self, ship, verb=False):
+        near = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1), (0, 0), (0, 1),
+            (1, -1), (1, 0), (1, 1)
+        ]
+
+        for dot in ship.dots:
+            for dx, dy in near:
+                cont = Dot(dot.x + dx, dot.y + dy)
+                if not (self.out(cont)) and cont not in self.busy:
+                    if verb:
+                        self.board[cont.x][cont.y] = "*"
+                    self.busy.append(cont)
+
 
     def shot(self, dot):
         # Выстрел в корабль
-        if self.out(dot):
+        if self.boardout(dot):
             raise BoardOutException()
         elif dot in self.busy:
             raise BoardUsedException()
-
         self.busy.append(dot)
-
         for ship in self.ships:
             if dot in ship.dots:
                 ship.hp -= 1
@@ -51,19 +92,13 @@ class Board:
                 if ship.hp == 0:
                     self.hp -= ship.length
                     self.contour(ship, verb=True)
-                    return False, "Корабль уничтожен"
+                    self.shipsunk += 1
+                    self.shipalive -= 1
+                    print(f"Корабль уничтожен. Осталось {self.shipalive} кораблей")
+                    return False
                 else:
-                    return True, "Корабль поврежден"
+                    return True, "Вижу дым на горизонте! Попадание!"
+        self.board[dot.x][dot.y] = "●"
+        return False, "Было близко, но мы промахнулись!"
 
-        self.board[dot.x][dot.y] = "*"
-        return False, "Мимо"
-
-
-class BoardOutException(Exception):
-    pass
-    # исключение ошибки, когда координаты за пределами доски
-
-
-class BoardUsedException(Exception):
-    pass
-    # исключение ошибки, когда координаты используются
+    def show_board(self):
