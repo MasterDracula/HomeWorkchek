dec30e64a6cb92e846be7bfb347a90cd914cd105
MasterDracula <IceClock@yandex.ru>
2024-08-01 21:27:06 +0300

    Delete типы кораблей.py

--- типы кораблей.py
+++
@@ -1,6 +0,0 @@
-def __str__(self):
-    board_view = "   | " + " | ".join(str(i) for i in range(1, self.size + 1)) + " |"
-    for i in range(self.size):
-        row = " | ".join(self.board[i])
-        board_view += f"\n {chr(ord('A') + i)} | {row} |"
-        print(board_view)
